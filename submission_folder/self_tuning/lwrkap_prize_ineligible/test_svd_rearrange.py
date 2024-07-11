25fd9971d4ad44b052ee01fdb31308963ba39b9f
davidtweedle <david.aig.tweedle@gmail.com>
2024-07-11 12:53:37 -0400

    Adjusting learning rate

--- submission_folder/self_tuning/lwrkap_prize_ineligible/test_svd_rearrange.py
+++ submission_folder/self_tuning/lwrkap_prize_ineligible/test_svd_rearrange.py
@@ -99,9 +99,9 @@ def init_optimizer_state(workload: spec.Workload,
   """
   global lrkaState
   if hyperparameters is None:
-    hparams_dict = {'learning_rate': 1,
+    hparams_dict = {'learning_rate': 0.5,
                     'start_factor': 0.1,
-                    'total_iters': 10,
+                    'total_iters': 100,
                     'momentum': 0.0,
                     'l2': 5e-4,
                     'svd_rank': 10,
