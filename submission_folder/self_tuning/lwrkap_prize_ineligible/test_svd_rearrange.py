6632a8ee640155d7a82d6bd439cddcd138a2c63f
davidtweedle <david.aig.tweedle@gmail.com>
2024-07-11 09:53:16 -0400

    Low learning rate for wmt

--- submission_folder/self_tuning/lwrkap_prize_ineligible/test_svd_rearrange.py
+++ submission_folder/self_tuning/lwrkap_prize_ineligible/test_svd_rearrange.py
@@ -99,9 +99,9 @@ def init_optimizer_state(workload: spec.Workload,
   """
   global lrkaState
   if hyperparameters is None:
-    hparams_dict = {'learning_rate': 0.25,
-                    'start_factor': 0.05,
-                    'total_iters': 100,
+    hparams_dict = {'learning_rate': 0.1,
+                    'start_factor': 0.1,
+                    'total_iters': 10,
                     'momentum': 0.0,
                     'l2': 5e-4,
                     'svd_rank': 10,
