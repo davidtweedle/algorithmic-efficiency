96b5764b6a4847fa78a1e4a28afcffbad5d9e12a
davidtweedle <david.aig.tweedle@gmail.com>
2024-07-12 20:14:47 -0400

    Fixing spacing error

--- submission_folder/self_tuning/lwrkap_prize_ineligible/test_svd_rearrange.py
+++ submission_folder/self_tuning/lwrkap_prize_ineligible/test_svd_rearrange.py
@@ -137,12 +137,11 @@ def init_optimizer_state(workload: spec.Workload,
           momentum=hyperparameters.momentum,
           weight_decay=hyperparameters.l2
           )
-
     def pytorch_cosine_warmup(int: step_hint, hyperparameters, optimizer):
-      # taken from prize_qualification_baselines/self_tuning/pytorch_nadamw_full_budget.py
-      warmup_steps = int(hyperparameters.warmup_factor * step_hint)
-      warmup = torch.optim.LinearLR(optimizer,
-                                    start_factor=1e-10,
+        # taken from prize_qualification_baselines/self_tuning/pytorch_nadamw_full_budget.py
+        warmup_steps = int(hyperparameters.warmup_factor * step_hint)
+        warmup = torch.optim.LinearLR(optimizer,
+                                     start_factor=1e-10,
                                     end_factor=1.,
                                     total_iters=warmup_steps
                                     )
@@ -152,6 +151,7 @@ def init_optimizer_state(workload: spec.Workload,
                                       schedulers=[warmup, cosine_decay],
                                       milestones=[warmup_steps]
                                       )
+
   scheduler = pytorch_cosine_warmup(workload.step_hint, hyperparameters, optimizer)
   optimizer_state = {
           'optimizer': optimizer,
