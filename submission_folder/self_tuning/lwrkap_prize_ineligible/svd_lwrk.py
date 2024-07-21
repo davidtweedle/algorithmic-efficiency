3b18e955987fa7339902f0b5f730bd33f224c9ee
davidtweedle <david.aig.tweedle@gmail.com>
2024-07-21 16:42:27 -0400

    Testing any timing difference between register_comm_hook and register_hook

--- submission_folder/self_tuning/lwrkap_prize_ineligible/svd_lwrk.py
+++ submission_folder/self_tuning/lwrkap_prize_ineligible/svd_lwrk.py
@@ -10,28 +10,29 @@ import collections
 from torch.optim.lr_scheduler import CosineAnnealingLR
 from torch.optim.lr_scheduler import LinearLR
 from torch.optim.lr_scheduler import SequentialLR
+from torch.optim.lr_scheduler import LambdaLR
+
+from functools import partial
 
 import torch.distributed as dist
 
 import numpy as np
 
 from absl import logging
-import optax
 import torch
-import torch.distributed.nn as dist_nn
-from torch.optim.lr_scheduler import LambdaLR
+
+import tensorly as tl
 
 from algorithmic_efficiency import spec
 from algorithmic_efficiency.pytorch_utils import pytorch_setup
 
 USE_PYTORCH_DDP, RANK, DEVICE, N_GPUS = pytorch_setup()
-lrkaState = None
+lrka_state = None
 # global variable to store the state of the communication hook
 # only used when calling this submission using
 # tuning_ruleset=external and using multiple tuning trials
 # if only using self tuning this is not needed
 
-import tensorly as tl
 tl.set_backend("pytorch")
 # used to find low rank approximations of gradient
 # using either the cp decomposition
@@ -101,17 +102,18 @@ def init_optimizer_state(workload: spec.Workload,
   optimizer state
   optimizer_update_fn
   """
-  global lrkaState
+  global lrka_state
   if hyperparameters is None:
-    hparams_dict = {'learning_rate': 1.0,
-                    'warmup_factor': 0.02,
+    hparams_dict = {'learning_rate': 1.,
+                    'warmup_factor': 0.05,
                     'momentum': 0.9,
-                    'l2': 5e-4,
-                    'svd_rank': 1,
-                    'upper_bound_factor': 20,
+                    'l2': 5e-5,
+                    'svd_rank': 5,
+                    'upper_bound_factor': 25,
                     'tol': 0.1,
                     'dropout_rate': 0.0,
-                    'aux_dropout_rate': 0.0
+                    'aux_dropout_rate': 0.0,
+                    'end_factor': 0.01
                     }
     hyperparameters = collections.namedtuple('Hyperparameters', hparams_dict)(**hparams_dict)
   random_state = int(rng[0])
@@ -127,26 +129,24 @@ def init_optimizer_state(workload: spec.Workload,
            'global_step': 0,
            'num_errs': 0
            }
-  if lrkaState is None:
-    lrkaState = LowRankApproximationState(**state)
-    model_params.register_comm_hook(lrkaState, comm_hook)
+  if lrka_state is None:
+    lrka_state = LowRankApproximationState(**state)
+    model_params.register_comm_hook(lrka_state, svd_hook)
     # register the communication hook which will
     # approximate the gradient on each gpu
     # then all reduce the results
   else:
-    lrkaState.__setstate__(state)
+    lrka_state.__set_state__(**state)
     # if this has been run using num_tuning_trials > 1
     # then we will need to re use the previous communication hook
-
-  for p in model_params.parameters():
-    print(p.shape)
+  
   optimizer = torch.optim.SGD(
           model_params.parameters(),
           lr=hyperparameters.learning_rate,
           momentum=hyperparameters.momentum,
           weight_decay=hyperparameters.l2
           )
-  def pytorch_cosine_warmup(step_hint: int, hyperparameters, optimizer):
+  def pytorch_linear_warmup(step_hint: int, hyperparameters, optimizer):
     # taken from prize_qualification_baselines/self_tuning/pytorch_nadamw_full_budget.py
     warmup_steps = int(hyperparameters.warmup_factor * step_hint)
     warmup = LinearLR(optimizer,
@@ -154,14 +154,18 @@ def init_optimizer_state(workload: spec.Workload,
                       end_factor=1.,
                       total_iters=warmup_steps
                       )
-    cosine_steps = max(step_hint - warmup_steps, 1)
-    cosine_decay = CosineAnnealingLR(optimizer, T_max=cosine_steps)
+    decay_steps = max(step_hint - warmup_steps, 1)
+    decay = LinearLR(optimizer,
+                     start_factor=1.,
+                     end_factor=hyperparameters.end_factor,
+                     total_iters=decay_steps
+                     )
     return SequentialLR(optimizer,
-                        schedulers=[warmup, cosine_decay],
+                        schedulers=[warmup, decay],
                         milestones=[warmup_steps]
                         )
 
-  scheduler = pytorch_cosine_warmup(workload.step_hint, hyperparameters, optimizer)
+  scheduler = pytorch_linear_warmup(workload.step_hint, hyperparameters, optimizer)
   optimizer_state = {
           'optimizer': optimizer,
           'scheduler': scheduler
@@ -187,8 +191,8 @@ def update_params(workload: spec.Workload,
      new_params
      new_model_state
     """
-  global lrkaState
-  lrkaState.setstep(global_step)
+  global lrka_state
+  lrka_state.setstep(global_step)
   # we need to note the global_step so
   # that on the first step we do not calculate the
   # svd of the gradients
@@ -309,28 +313,27 @@ def data_selection(workload: spec.Workload,
     """
   return next(input_queue)
 
-def comm_hook(state: LowRankApproximationState, bucket: dist.GradBucket) -> torch.futures.Future[torch.Tensor]:
-  if state.getstep() > 1:
+def svd_hook(lrka_state: LowRankApproximationState, bucket):
+  if lrka_state.get_step() > 1:
     for grad in bucket.gradients():
-      grad_shape = grad.shape
-      reshaped_grad = grad.reshape(grad_shape[0],-1)
-      if (len(reshaped_grad.shape) == 2 and reshaped_grad.shape[0] > 1 
-          and reshaped_grad.shape[1] > 1):
+      oldshape = grad.shape
+      reshaped_grad = grad.reshape(oldshape[0], -1)
+      m, n, _ = *reshaped_grad.shape, 1
+      upper_rank = min(m, n, lrka_state.upper_bound_rank)
+      rank = min(upper_rank, lrka_state.svd_rank)
+      if upper_rank > 1:
         try:
-          rank = state.upper_bound_rank if state.upper_bound_rank < reshaped_grad.shape[0] else reshaped_grad.shape[0]
-          rank = rank if rank < reshaped_grad.shape[1] else reshaped_grad.shape[1] 
-          U,S,V = torch.svd_lowrank(
+          U, S, V = torch.svd_lowrank(
                   reshaped_grad,
-                  q=rank
+                  q=upper_rank
                   )
-          rank = state.svd_rank if state.svd_rank < rank else rank
-          U = U[:,:rank]
+          U = U[:, :rank]
           S = S[:rank]
-          V = V[:,:rank]
+          V = V[:, :rank]
           reshaped_grad = (U * S) @ V.T
         except torch._C._LinAlgError as err:
-          state.num_errs += 1
-          logging.info('Communication hook threw error number ' + str(state.num_errs))
-      reshaped_grad.div_(state.n_gpus)
-      grad = reshaped_grad.reshape(grad_shape)
+          lrka_state.num_errs += 1
+          logging.info(f'Communication hook threw error number {lrka_state.num_errs}')
+      grad = reshaped_grad.reshape(*oldshape)
+      grad.div_(lrka_state.n_gpus)
   return dist.all_reduce(bucket.buffer(), async_op=True).get_future().then(lambda fut: fut.value()[0])
