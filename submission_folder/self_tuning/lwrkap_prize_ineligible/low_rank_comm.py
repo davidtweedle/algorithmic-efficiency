d69917bd9aac1ea40196a47e586eb2dbd7598bb0
davidtweedle <david.aig.tweedle@gmail.com>
2024-09-08 20:41:30 -0400

    Experimenting with eps

--- submission_folder/self_tuning/lwrkap_prize_ineligible/low_rank_comm.py
+++ submission_folder/self_tuning/lwrkap_prize_ineligible/low_rank_comm.py
@@ -156,7 +156,7 @@ def lwrk_hook(state: LowRankApproximationState, bucket):
         Y = torch.bmm(tensor, u)
         v = torch.randn(batch_size, state.matrix_approximation_rank, m, device=device)
         middle, X = torch.bmm(v, torch.cat((Y, tensor), dim=2)).split([state.matrix_approximation_rank, n], 2)
-        u, s, v = torch.svd(middle)
+        u, s, v = torch.linalg.svd(middle)
         s = torch.where(s > state.eps, s ** -1, torch.zeros_like(s))
         v = torch.bmm(v, torch.diag_embed(s, dim1=-2, dim2=-1))
         Y = torch.bmm(Y, v)
