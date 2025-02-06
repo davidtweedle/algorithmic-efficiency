e2e735821acdc58bde515fd1cffb848e6530582d
David Tweedle <david.aig.tweedle@gmail.com>
2025-02-06 10:21:30 -0500

    Testing fully_shard

--- algorithmic_efficiency/workloads/cifar/cifar_pytorch/workload.py
+++ algorithmic_efficiency/workloads/cifar/cifar_pytorch/workload.py
@@ -137,13 +137,10 @@ class CifarWorkload(BaseCifarWorkload):
     self._model.to(DEVICE)
     if N_GPUS > 1:
       if USE_PYTORCH_DDP:
-        for module in self._model.modules():
-          try:
-            fully_shard(module)
-          except AssertionError as e:
-            logging.info(f"Caught {e}, on module {module}")
-            raise
-        self._model = fully_shard(self._model)
+        for module in self._model.modules()[::-1]
+          logger.info(f"Module {module}, Module weights, {module.weight}")
+          fully_shard(module)
+        #self._model = fully_shard(self._model)
       else:
         self._model = torch.nn.DataParallel(self._model)
     return self._model, None
